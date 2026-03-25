671413bd6b4b90ce15316a34dba336d659136a6d
root <root@vmi2634684.contaboserver.net>
2026-03-25 12:12:35 +0100

    feat: add reachout timelock types and 463 NACK constant

--- src/Types/Newsletter.ts
+++ src/Types/Newsletter.ts
@@ -10,7 +10,9 @@ export enum XWAPaths {
 	xwa2_newsletter_unfollow = 'xwa2_newsletter_unfollow',
 	xwa2_newsletter_change_owner = 'xwa2_newsletter_change_owner',
 	xwa2_newsletter_demote = 'xwa2_newsletter_demote',
-	xwa2_newsletter_delete_v2 = 'xwa2_newsletter_delete_v2'
+	xwa2_newsletter_delete_v2 = 'xwa2_newsletter_delete_v2',
+	xwa2_fetch_account_reachout_timelock = 'xwa2_fetch_account_reachout_timelock',
+	xwa2_message_capping_info = 'xwa2_message_capping_info'
 }
 export enum QueryIds {
 	CREATE = '8823471724422422',
